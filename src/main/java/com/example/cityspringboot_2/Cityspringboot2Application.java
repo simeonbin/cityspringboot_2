863f1012d7fd8d949c5fe681cb616e1c079107c3
BINIATIDIS Symeon <symeon.biniatidis@ext.ec.europa.eu>
2019-08-07 15:10:22 +0200

    1st commit

---
+++ src/main/java/com/example/cityspringboot_2/Cityspringboot2Application.java
@@ -0,0 +1,13 @@
+package com.example.cityspringboot_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cityspringboot2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cityspringboot2Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/cityspringboot_2/ICityService.java
@@ -0,0 +1,10 @@
+package com.example.cityspringboot_2;
+
+
+import java.util.List;
+
+public interface ICityService {
+
+    public List<City> findAll();
+}
+
